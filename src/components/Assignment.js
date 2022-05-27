650d52a078b472169c40e6a430c57bbdb1082e49
Tom Wooten <73683475+wootent@users.noreply.github.com>
2022-05-27 11:25:08 -0700

    assignment 3

--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -22,6 +22,7 @@ class Assignment extends React.Component {
     this.fetchAssignments();
   }
  
+ // fetch assignments
   fetchAssignments = () => {
     console.log("Assignment.fetchAssignments");
     const token = Cookies.get('XSRF-TOKEN');
@@ -33,6 +34,7 @@ class Assignment extends React.Component {
     .then((response) => response.json()) 
     .then((responseData) => { 
       if (Array.isArray(responseData.assignments)) {
+        console.log(responseData.assignments);
         //  add to each assignment an "id"  This is required by DataGrid  "id" is the row index in the data grid table 
         this.setState({ assignments: responseData.assignments.map((assignment, index) => ( { id: index, ...assignment } )) });
       } else {
@@ -44,9 +46,56 @@ class Assignment extends React.Component {
     .catch(err => console.error(err)); 
   }
   
+   // when create assignment button pressed, send POST, assignment to back end 
+    handleCreate = ( ) => {
+      console.log("Assignment.handleCreate");
+      const token = Cookies.get('XSRF-TOKEN');
+      
+      fetch(`${SERVER_URL}/course/${this.state.courseId}/assignment`, 
+          {  
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json',
+                       'X-XSRF-TOKEN': token }, 
+            body: JSON.stringify({assignmentName: this.state.name,  dueDate: this.state.dueDate, courseId: this.state.courseId})
+          } )
+      .then(res => {
+          if (res.ok) {
+            toast.success("Assignment Created Successfully ", {
+            position: toast.POSITION.BOTTOM_LEFT
+            });
+            this.fetchAssignments();
+          } else {
+            toast.error("Create Assignment failed", {
+            position: toast.POSITION.BOTTOM_LEFT
+            });
+            console.error('Put http status =' + res.status);
+      }})
+        .catch(err => {
+          toast.error("Create Assignment failed", {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error(err);
+        });
+   };        
+  
    onRadioClick = (event) => {
     console.log("Assignment.onRadioClick " + event.target.value);
     this.setState({selected: event.target.value});
+  }
+
+  onNameChange = (e) => {
+    console.log("Assignment.onNameChange " + e.target.value);
+    this.setState({name: e.target.value});
+  }
+
+  onDateChange = (e) => {
+    console.log("Assignment.onDateChange " + e.target.value);
+    this.setState({dueDate: e.target.value});
+  }
+
+  onCourseIdChange = (e) => {
+    console.log("Assignment.onCourseIDChange " + e.target.value);
+    this.setState({courseId: e.target.value});
   }
   
   render() {
@@ -58,7 +107,7 @@ class Assignment extends React.Component {
         renderCell: (params) => (
           <div>
           <Radio
-            checked={params.row.id == this.state.selected}
+            checked={params.row.id === this.state.selected}
             onChange={this.onRadioClick}
             value={params.row.id}
             color="default"
@@ -69,6 +118,7 @@ class Assignment extends React.Component {
         )
       },
       { field: 'courseTitle', headerName: 'Course', width: 300 },
+      { field: 'courseId', headerName: 'Course ID', width: 200 },
       { field: 'dueDate', headerName: 'Due Date', width: 200 }
       ];
       
@@ -78,11 +128,28 @@ class Assignment extends React.Component {
             <h4>Assignment(s) ready to grade: </h4>
               <div style={{ height: 450, width: '100%', align:"left"   }}>
                 <DataGrid rows={this.state.assignments} columns={columns} />
-              </div>                
-            <Button component={Link} to={{pathname:'/gradebook',   assignment: assignmentSelected }} 
-                    variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
-              Grade
-            </Button>
+              </div>
+              <div>
+              	<Button component={Link} to={{pathname:'/gradebook',   assignment: assignmentSelected }} 
+              		variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
+              	Grade
+              	</Button>
+                <hr></hr>
+              </div>
+              <div align="center">
+                <h3>Create Assigments</h3>
+                <label for="assignmentName">Name: </label>
+                <input type="text" id="assignmentName" name="assignmentName" onChange={this.onNameChange}></input><br></br>
+                <label for="assignmentDueDate">Date Due: </label>
+                <input type="text" id="dueDate" name="dueDate" onChange={this.onDateChange}></input><br></br>
+                <label for="courseId">Course ID: </label>
+                <input type="number" id="courseId" name="courseId" onChange={this.onCourseIdChange}></input><br></br>
+              </div>
+              <div align="center">
+                <Button variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleCreate} >
+                   Create Assignment
+                </Button>
+              </div>                              
             <ToastContainer autoClose={1500} /> 
           </div>
       )
